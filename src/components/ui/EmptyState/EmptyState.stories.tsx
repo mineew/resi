e7b313115f5b144f3570c10b401b4069248a1ec2
Mineev Alexander <mineew.al@gmail.com>
2024-04-30 14:49:55 +0300

    added EmptyState component

---
+++ src/components/ui/EmptyState/EmptyState.stories.tsx
@@ -0,0 +1,25 @@
+import { type Meta, type StoryFn } from '@storybook/react';
+import { Inbox } from 'lucide-react';
+
+import EmptyState from './EmptyState';
+
+export const Default: StoryFn<typeof EmptyState> = () => {
+  return (
+    <div style={{ height: '100vh' }}>
+      <EmptyState icon={<Inbox />}>
+        <p>Empty State Content</p>
+        <p>Empty State Content</p>
+        <p>Empty State Content</p>
+      </EmptyState>
+    </div>
+  );
+};
+
+export default {
+  title: 'Components/UI/EmptyState',
+  component: EmptyState,
+  argTypes: {
+    icon: { table: { disable: true } },
+    children: { table: { disable: true } },
+  },
+} as Meta<typeof EmptyState>;
---
+++ src/components/ui/EmptyState/EmptyState.tsx
@@ -0,0 +1,23 @@
+import { type ReactNode } from 'react';
+
+import styles from './EmptyState.module.css';
+
+interface EmptyStateProps {
+  icon: ReactNode;
+  children: ReactNode;
+}
+
+function EmptyState(props: EmptyStateProps) {
+  const { icon, children } = props;
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.body}>
+        <div className={styles.icon}>{icon}</div>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default EmptyState;
