a67d8ba2ece1e273986dfe1aaa3fff2174309382
Mineev Alexander <mineew.al@gmail.com>
2024-05-03 12:55:26 +0300

    improve vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import path from 'path';
 
+import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    sourcemap: true,
+
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+
+        assetFileNames: ({ name }) => {
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]';
+          }
+
+          if (/\.(woff2?|ttf)$/.test(name ?? '')) {
+            return 'assets/fonts/[name]-[hash][extname]';
+          }
+
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
+
+  css: {
+    devSourcemap: true,
+  },
+
   resolve: {
     alias: {
       '~': path.resolve(__dirname, '.'),
@@ -11,5 +39,5 @@ export default defineConfig({
     },
   },
 
-  plugins: [react()],
+  plugins: [react(), legacy()],
 });
