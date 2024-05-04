ecede926b1b15326ba1bc021fb9cea751d5a33de
Mineev Alexander <mineew.al@gmail.com>
2024-05-04 19:35:02 +0300

    added test coverage config

--- src/utils/arrays/splitArrayOnChunks.test.ts
+++ src/utils/arrays/splitArrayOnChunks.test.ts
@@ -1,10 +1,12 @@
-import { expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import splitArrayOnChunks from './splitArrayOnChunks';
 
-test('@/utils/splitArrayOnChunks', () => {
-  const array = [1, 2, 3, 4, 5];
-  const chunks = splitArrayOnChunks(array, 2);
+describe('@/utils/splitArrayOnChunks', () => {
+  it('should split an array on chunks', () => {
+    const array = [1, 2, 3, 4, 5];
+    const chunks = splitArrayOnChunks(array, 2);
 
-  expect(chunks).toStrictEqual([[1, 2], [3, 4], [5]]);
+    expect(chunks).toStrictEqual([[1, 2], [3, 4], [5]]);
+  });
 });
