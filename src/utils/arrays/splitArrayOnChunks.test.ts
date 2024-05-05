65f2788eb65efe234d3f8adc5c57b4f7057acce6
Mineev Alexander <mineew.al@gmail.com>
2024-05-05 09:19:17 +0300

    configure vitest globals

--- src/utils/arrays/splitArrayOnChunks.test.ts
+++ src/utils/arrays/splitArrayOnChunks.test.ts
@@ -1,5 +1,3 @@
-import { describe, expect, it } from 'vitest';
-
 import splitArrayOnChunks from './splitArrayOnChunks';
 
 describe('@/utils/splitArrayOnChunks', () => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      globals: true,
       coverage: {
         enabled: true,
         reporter: ['html'],
