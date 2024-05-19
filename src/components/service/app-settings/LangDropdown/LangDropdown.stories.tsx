52365f3a38ea8d1ebc2ffd49ba4dbb91d7ea42d1
Mineev Alexander <mineew.al@gmail.com>
2024-05-19 08:45:36 +0300

    added LangDropdown service component

---
+++ src/components/service/app-settings/LangDropdown/LangDropdown.stories.tsx
@@ -0,0 +1,39 @@
+import { type Meta, type StoryFn } from '@storybook/react';
+import { useState } from 'react';
+
+import LangDropdown, { type LangDropdownItem } from './LangDropdown';
+
+export const Default: StoryFn<typeof LangDropdown> = () => {
+  const [selectedLang, setSelectedLang] = useState('en');
+
+  const langs: LangDropdownItem[] = [
+    {
+      id: 'en',
+      label: 'English',
+    },
+    {
+      id: 'ru',
+      label: 'Русский',
+    },
+  ];
+
+  return (
+    <div style={{ padding: 20 }}>
+      <LangDropdown
+        langs={langs}
+        selectedLang={selectedLang}
+        onChangeSelectedLang={setSelectedLang}
+      />
+    </div>
+  );
+};
+
+export default {
+  title: 'Components/Service/App Settings/LangDropdown',
+  component: LangDropdown,
+  argTypes: {
+    langs: { table: { disable: true } },
+    selectedLang: { table: { disable: true } },
+    onChangeSelectedLang: { table: { disable: true } },
+  },
+} as Meta<typeof LangDropdown>;
---
+++ src/components/service/app-settings/LangDropdown/LangDropdown.tsx
@@ -0,0 +1,37 @@
+import { memo } from 'react';
+
+import Dropdown, { type DropdownItem } from '@/components/ui/Dropdown/Dropdown';
+
+interface LangDropdownItem {
+  id: string;
+  label: string;
+}
+
+interface LangDropdownProps {
+  langs: LangDropdownItem[];
+  selectedLang: string;
+  onChangeSelectedLang: (lang: string) => void;
+}
+
+const LangDropdown = memo((props: LangDropdownProps) => {
+  const { langs, selectedLang, onChangeSelectedLang } = props;
+
+  const dropdownItems: DropdownItem[] = langs.map((lang) => ({
+    id: lang.id,
+    label: lang.label,
+    selected: lang.id === selectedLang,
+    onClick: () => onChangeSelectedLang(lang.id),
+  }));
+
+  return (
+    <Dropdown
+      items={dropdownItems}
+      defaultTriggerTitle={selectedLang.toUpperCase()}
+    />
+  );
+});
+
+LangDropdown.displayName = 'LangDropdown';
+
+export default LangDropdown;
+export type { LangDropdownItem };
