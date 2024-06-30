d95a615f8145fcc0af2a97ff8aaf893c17904259
Mineev Alexander <mineew.al@gmail.com>
2024-06-30 09:00:37 +0300

    Button component: added unit test

---
+++ src/components/ui/Button/Button.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+
+import Button from './Button';
+
+describe('@/components/ui/Button', () => {
+  it('renders', () => {
+    render(<Button>Button Text</Button>);
+
+    const button = screen.getByRole('button', { name: 'Button Text' });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('can be clicked', async () => {
+    const user = userEvent.setup();
+    const handleClick = vi.fn();
+
+    render(<Button onClick={handleClick}>Click Me</Button>);
+
+    const button = screen.getByRole('button', { name: 'Click Me' });
+    expect(button).toBeInTheDocument();
+
+    await user.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('can be disabled', async () => {
+    const user = userEvent.setup();
+    const handleClick = vi.fn();
+
+    render(
+      <Button onClick={handleClick} disabled>
+        Disabled
+      </Button>,
+    );
+
+    const button = screen.getByRole('button', { name: 'Disabled' });
+    expect(button).toBeInTheDocument();
+
+    await user.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(0);
+  });
+});
