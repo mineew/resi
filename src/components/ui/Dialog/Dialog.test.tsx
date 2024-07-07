64476ccecc57761357c10ef7b92439c8bf177666
Mineev Alexander <mineew.al@gmail.com>
2024-07-07 18:45:28 +0300

    Dialog component: added unit test

---
+++ src/components/ui/Dialog/Dialog.test.tsx
@@ -0,0 +1,115 @@
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+
+import Dialog from './Dialog';
+
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+beforeAll(() => {
+  vi.stubGlobal('ResizeObserver', ResizeObserverMock);
+
+  return () => {
+    vi.unstubAllGlobals();
+  };
+});
+
+describe('@/components/ui/Dialog', () => {
+  it('renders', () => {
+    render(
+      <Dialog
+        open
+        title="Dialog Title"
+        trigger={<button type="button">Open Dialog</button>}
+      >
+        <p>Dialog Content</p>
+      </Dialog>,
+    );
+
+    const dialog = screen.getByRole('dialog');
+    expect(dialog).toBeInTheDocument();
+  });
+
+  it('can be closed', async () => {
+    const user = userEvent.setup();
+    const handleOpenChange = vi.fn();
+
+    render(
+      <Dialog
+        open
+        title="Dialog Title"
+        trigger={<button type="button">Open Dialog</button>}
+        onOpenChange={handleOpenChange}
+      >
+        <p>Dialog Content</p>
+      </Dialog>,
+    );
+
+    const closeButton = screen.getByRole('button');
+    await user.click(closeButton);
+
+    expect(handleOpenChange).toHaveBeenCalledTimes(1);
+    expect(handleOpenChange).toHaveBeenCalledWith(false);
+  });
+
+  it('can be closed by ESC', async () => {
+    const user = userEvent.setup();
+    const handleOpenChange = vi.fn();
+
+    render(
+      <Dialog
+        open
+        title="Dialog Title"
+        trigger={<button type="button">Open Dialog</button>}
+        tooltip="Dialog Trigger Tooltip"
+        onOpenChange={handleOpenChange}
+      >
+        <p>Dialog Content</p>
+      </Dialog>,
+    );
+
+    await user.keyboard('{Escape}');
+
+    expect(handleOpenChange).toHaveBeenCalledTimes(1);
+    expect(handleOpenChange).toHaveBeenCalledWith(false);
+  });
+
+  it('can display footer', () => {
+    render(
+      <Dialog
+        open
+        title="Dialog Title"
+        trigger={<button type="button">Open Dialog</button>}
+        footer={<p>Footer Content</p>}
+      >
+        <p>Dialog Content</p>
+      </Dialog>,
+    );
+
+    const footer = screen.getByText('Footer Content');
+    expect(footer).toBeInTheDocument();
+  });
+
+  it('can display tooltip', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Dialog
+        title="Dialog Title"
+        trigger={<button type="button">Open Dialog</button>}
+        tooltip="Dialog Trigger Tooltip"
+      >
+        <p>Dialog Content</p>
+      </Dialog>,
+    );
+
+    const button = screen.getByRole('button');
+    await user.hover(button);
+
+    const tooltip = screen.getByRole('tooltip');
+    expect(tooltip).toHaveTextContent('Dialog Trigger Tooltip');
+  });
+});
