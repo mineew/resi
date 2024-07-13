ac27882e95d0019d353ad02a5561a3e5dfa334cc
Mineev Alexander <mineew.al@gmail.com>
2024-07-13 22:54:23 +0300

    LineChart: improve unit test

--- src/components/charts/LineChart/LineChart.test.tsx
+++ src/components/charts/LineChart/LineChart.test.tsx
@@ -36,25 +36,51 @@ const data: LineChartDataset[] = [
   },
   {
     name: 'Dataset 2',
-    contents: [1, 2, 3, 4, 5],
-    color: '#FF0000',
-    strokeWidth: 1,
+    contents: [6, 7, 8, 9, 10],
+    color: '#00FF00',
+    strokeWidth: 2,
   },
   {
     name: 'Dataset 3',
-    contents: [1, 2, 3, 4, 5],
-    color: '#FF0000',
-    strokeWidth: 1,
+    contents: [11, 12, 13, 14, 15],
+    color: '#0000FF',
+    strokeWidth: 3,
   },
 ];
 
 describe('@/components/charts/LineChart', () => {
   it('renders', () => {
     render(
-      <LineChart title="Line Chart" data={data} width={1000} height={1000} />,
+      <LineChart
+        title="Line Chart"
+        data={data}
+        xConverter={(x) => x + 1}
+        xTickCount={10}
+        offsetGap={1}
+        offsetLeft={2}
+        offsetRight={10}
+        width={1000}
+        height={1000}
+      />,
     );
 
     const container = screen.getByTestId('line-chart-container');
     expect(container).toBeInTheDocument();
+  });
+
+  it('can render axis labels', () => {
+    render(
+      <LineChart
+        title="Line Chart"
+        data={data}
+        xLabel="X Label"
+        yLabel="Y Label"
+        width={1000}
+        height={1000}
+      />,
+    );
+
+    const xLabel = screen.getByText('X Label');
+    expect(xLabel).toBeInTheDocument();
   });
 });
