c666a60e703ebf012050eb5cd8479a5dd41bbfa7
Mineev Alexander <mineew.al@gmail.com>
2024-07-25 12:10:00 +0300

    fetchExampleFiles util: fix file url

--- src/utils/resi-files/fetchExampleFiles.ts
+++ src/utils/resi-files/fetchExampleFiles.ts
@@ -5,7 +5,11 @@ async function fetchExampleFiles() {
     for (let i = 0; i < 10; i += 1) {
       const fileN = i + 1 <= 9 ? `0${i + 1}` : (i + 1).toString();
       const fileName = `File-0${fileN}`;
-      const file = await fetch(`app/example-data/${fileName}.xls`);
+      const filePath = import.meta.env.DEV
+        ? `app/example-data/${fileName}.xls`
+        : `example-data/${fileName}.xls`;
+
+      const file = await fetch(filePath);
       const fileContents = await file.text();
 
       result[fileName] = fileContents;
