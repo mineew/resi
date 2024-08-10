c781731cc3635c14d1c015b12a2b17715b020099
Mineev Alexander <mineew.al@gmail.com>
2024-08-10 09:15:52 +0300

    exportChartToPng: added unit test

---
+++ src/utils/svg-to-png/exportChartToPng.test.ts
@@ -0,0 +1,27 @@
+import exportChartToPng from './exportChartToPng';
+
+const mocks = vi.hoisted(() => ({
+  toBlob: vi.fn(() => Promise.resolve(new Blob())),
+  downloadBlob: vi.fn(),
+}));
+
+vi.mock('html-to-image', () => ({
+  toBlob: mocks.toBlob,
+}));
+
+vi.mock('./downloadBlob', () => ({
+  default: mocks.downloadBlob,
+}));
+
+describe('@/utils/svg-to-png/exportChartToPng', () => {
+  it('exports chart to png', async () => {
+    const chartWrapper = document.createElement('div');
+    const svgElement = document.createElement('svg');
+    chartWrapper.append(svgElement);
+
+    await exportChartToPng(chartWrapper, 'image.png', 1);
+
+    expect(mocks.toBlob).toHaveBeenCalledTimes(1);
+    expect(mocks.downloadBlob).toHaveBeenCalledTimes(1);
+  });
+});
