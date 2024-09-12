7cac779c870f458ffc510ee56cd65aa227dea733
Mineev Alexander <mineew.al@gmail.com>
2024-09-12 09:24:33 +0300

    AppLayout: removed sidebar toggle button

--- src/components/layout/AppLayout/AppLayout.test.tsx
+++ src/components/layout/AppLayout/AppLayout.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event';
 
 import AppLayout from './AppLayout';
 
@@ -28,24 +27,5 @@ describe('@/components/layout/AppLayout', () => {
 
     const growthChart = screen.getByText('File Growth Chart');
     expect(growthChart).toBeInTheDocument();
-  });
-
-  it('can toggle sidebar', async () => {
-    render(
-      <AppLayout
-        fileList={<div>File List</div>}
-        fileChart={<div>File Chart</div>}
-        diffChart={<div>File Diff Chart</div>}
-        growthChart={<div>File Growth Chart</div>}
-      />,
-    );
-
-    const expandedButton = screen.getByRole('button', { expanded: true });
-    expect(expandedButton).toBeInTheDocument();
-
-    await userEvent.click(expandedButton);
-
-    const collapsedButton = screen.getByRole('button', { expanded: false });
-    expect(collapsedButton).toBeInTheDocument();
   });
 });
--- src/components/layout/AppLayout/AppLayout.tsx
+++ src/components/layout/AppLayout/AppLayout.tsx
@@ -1,8 +1,6 @@
-import classNames from 'classnames';
-import { type ReactNode, useCallback, useState } from 'react';
+import { type ReactNode } from 'react';
 
 import styles from './AppLayout.module.css';
-import AppLayoutSidebarToggleButton from './AppLayoutSidebarToggleButton';
 
 interface AppLayoutProps {
   fileList: ReactNode;
@@ -13,30 +11,10 @@ interface AppLayoutProps {
 
 function AppLayout(props: AppLayoutProps) {
   const { fileList, fileChart, diffChart, growthChart } = props;
-  const [fileListIsCollapsed, setFileListIsCollapsed] = useState(false);
-
-  const fileListId = 'resi-file-list';
-
-  const handleToggleFileList = useCallback(() => {
-    setFileListIsCollapsed((collapsed) => !collapsed);
-  }, []);
 
   return (
     <div className={styles.wrapper} data-testid="app-layout">
-      <div
-        className={classNames(styles['file-list'], {
-          [styles.collapsed]: fileListIsCollapsed,
-        })}
-        id={fileListId}
-      >
-        {fileList}
-      </div>
-
-      <AppLayoutSidebarToggleButton
-        isCollapsed={fileListIsCollapsed}
-        controls={fileListId}
-        onClick={handleToggleFileList}
-      />
+      <div className={styles['file-list']}>{fileList}</div>
 
       <div className={styles.charts}>
         <div className={styles['file-chart']}>{fileChart}</div>
